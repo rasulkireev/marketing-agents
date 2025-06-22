f627d155b9740d9a358a7107601c9b2d76db8941
Rasul Kireev <me@rasulkireev.com>
2025-06-22 11:39:46 -0300

    init

--- core/utils.py
+++ core/utils.py
@@ -19,21 +19,22 @@ class DivErrorList(ErrorList):
               <div class="flex">
                 <div class="flex-shrink-0">
                   <!-- Heroicon name: solid/x-circle -->
-                  <svg class="w-5 h-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"> # noqa: E501
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" /> # noqa: E501
+                  <svg class="w-5 h-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                   </svg>
                 </div>
                 <div class="ml-3 text-sm text-red-700">
-                      {''.join([f'<p>{e}</p>' for e in self])}
+                      {"".join([f"<p>{e}</p>" for e in self])}
                 </div>
               </div>
             </div>
-         """
+         """  # noqa: E501
 
 
 def replace_placeholders(data, blog_post):
     """
-    Recursively replace values in curly braces (e.g., '{{ slug }}') in a dict with the corresponding attribute from blog_post.
+    Recursively replace values in curly braces (e.g., '{{ slug }}')
+    in a dict with the corresponding attribute from blog_post.
     """
     if isinstance(data, dict):
         return {k: replace_placeholders(v, blog_post) for k, v in data.items()}
@@ -75,8 +76,8 @@ def get_or_create_project(profile_id, url, source=None):
             event="project_created",
             properties=project_metadata,
         )
-        logger.info("[get_or_create_project] Project created", **project_metadata)
+        logger.info("[Get or Create Project] Project created", **project_metadata)
     else:
-        logger.info("[get_or_create_project] Got existing project", **project_metadata)
+        logger.info("[Get or Create Project] Got existing project", **project_metadata)
 
     return project
