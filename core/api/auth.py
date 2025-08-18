a02e456019823d6d7d09c205215ca353d06f1860
Rasul Kireev <me@rasulkireev.com>
2025-08-18 10:57:17 +0300

    order of auth

--- core/api/auth.py
+++ core/api/auth.py
@@ -15,17 +15,17 @@ class MultipleAuthSchema(HttpBearer):
             token=token,
         )
 
-        # For session-based authentication (when using the web interface)
-        if hasattr(request, "user") and request.user.is_authenticated:
-            try:
-                return request.user.profile
-            except Profile.DoesNotExist:
-                return None
-
         # For API token authentication (when using the API directly)
         if token:
             try:
                 return Profile.objects.get(key=token)
+            except Profile.DoesNotExist:
+                return None
+
+        # For session-based authentication (when using the web interface)
+        if hasattr(request, "user") and request.user.is_authenticated:
+            try:
+                return request.user.profile
             except Profile.DoesNotExist:
                 return None
 
